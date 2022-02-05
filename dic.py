9195f517c62fe1a8e656e0b34ee53612717588b3
harrybedi22 <harshbedi90@gmail.com>
2022-02-05 23:33:01 +0530

    first commit

---
+++ dic.py
@@ -0,0 +1,37 @@
+pet1 = {'doggy':'White'}
+pet2 = {'doggy2':'black'}
+
+pet1['doggy']="green"
+
+pet = [pet1,pet2]
+
+for value in pet:
+    print(value)
+
+
+# rivers ={'nile':'egypt','ganga':'india','chenab':'pakistan'}
+
+# for key, value in rivers.items():
+#     print(f"{key} runs in {value}")
+
+# for key in rivers.keys():
+#     print(key)
+# for value in rivers.values():
+#     print(value)
+
+# Person ={'first_name':"Harsh",'last_name':"Bedi","age":31,"city":"Pathankot"}
+
+# for key,value in Person.items():
+#     print(f"Key , value = {key},{value}")
+
+
+# print(f"First Name is {Person['first_name']}")
+# print(f"Last Name is {Person['last_name']}")
+# print(f"Age is {Person['age']}")
+# print(f"City Name is {Person['city']}")
+# print(alien_0['color'])
+# print(alien_0['points'])
+
+# names = {'Harsh':31,"Jayveer":3}
+# print(names["Harsh"])
+# print(names(0))
---
+++ forloop.py
@@ -0,0 +1,77 @@
+#### Ordinal Number from 1 to 9
+
+for i in range(1,10):
+    if i ==1:
+        print(f"{i}st")
+    elif i==2:
+        print(f"{i}nd")
+    elif i==3:
+        print(f"{i}rd")
+    else:
+        print(f"{i}th")
+
+
+
+
+# Check user name.
+# current_users = ["harsh","BEdi","Harry","admin"]
+# new_users = ["Jayveer","Bedi"]
+
+# lowercurrent_user = []
+# for value in current_users:
+#     lowercurrent_user.append(value.lower())
+# print(lowercurrent_user)
+# for user in new_users:
+#     if user.lower in lowercurrent_user:
+#         print(f"{user},Username already taken")
+#     else:
+#         print(f"{user}, is a valid username")
+
+
+# food = ("rice","dal","salad","water","idli")
+# food[0]= "sambar"
+#     print(foods)
+
+
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# friend_foods = my_foods[:]
+# my_foods.append('cannoli')
+# friend_foods.append('ice cream')
+# print(friend_foods)
+# print(my_foods)
+
+
+
+# #odd numbers
+# cube = list(value**3 for value in range(1,11))
+# print(cube)
+# list1 = list(range(1,1000001))
+# print(min(list1))
+# print(max(list1))
+# print(sum(list1))
+
+# digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print(min(digits))
+# print(max(digits))
+# print(sum(digits))
+
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     print(magician)
+
+# number = list(range(1,5))
+# print(number)
+
+# All even number between 1 to 100
+
+# evennumb = list(range(2,101,2))
+# print(evennumb)
+
+# Print a square valued list from 1 to 10
+# squares =[]
+# for square in range(1,11):
+#     value = square**2
+#     squares.append(value)
+
+# print(squares)
